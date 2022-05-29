b90453372c441ed0492ed6b0816736d113a1de5b
Andre Zimpel <andrezimpel@users.noreply.github.com>
2022-05-29 16:42:02 +0200

    code is working now :-)

--- src/lambda/validate-code.js
+++ src/lambda/validate-code.js
@@ -1,33 +1,51 @@
-exports.handler = async (event, context, callback) => {
-  // fetch('https://www.learnwithjason.dev/graphql', {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //   },
-  //   body: JSON.stringify({
-  //     query: `
-  //         query GetLearnWithJasonEpisodes($now: DateTime!) {
-  //           allEpisode(limit: 10, sort: {date: ASC}, where: {date: {gte: $now}}) {
-  //             date
-  //             title
-  //             guest {
-  //               name
-  //               twitter
-  //             }
-  //             description
-  //           }
-  //         }
-  //       `,
-  //     variables: {
-  //       now: new Date().toISOString(),
-  //     },
-  //   }),
-  // })
-  //   .then((res) => res.json())
-  //   .then((result) => console.log(result));
-  return callback(null, {
-    statusCode: 200,
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ code: "code", url: "routes[item].data.destination" })
-  })
-};
+import axios from 'axios'
+
+export async function handler(event, context) {
+  try {
+    const response = await axios.post('https://10kmore-com.myshopify.com/admin/api/2022-04/graphql.json', JSON.stringify({
+      query: `
+          query ProductQuery($id: ID!) {
+            product(id: $id){
+              title
+              handle
+              metafield(namespace:"my_fields", key: "access_code") {
+                key
+                value
+              }
+            }
+          }
+        `,
+      variables: {
+        id: 'gid://shopify/Product/7284539490461'
+      },
+    }), {
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Access-Token": "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"
+      }
+    })
+
+    const { product } = response.data.data
+    const responseCode = product.metafield.value
+
+    const code = event.queryStringParameters.code
+    let codeResponse = { access: "denied" }
+
+    if (responseCode.toLowerCase() === code.toLowerCase()) {
+      codeResponse = {
+    		access: "granted", url: `/products/${product.handle}?code=${responseCode}`, code: responseCode
+    	}
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(codeResponse)
+    }
+  } catch (err) {
+    console.log(err) // output to netlify function log
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ access: "denied" }) // Could be a custom message or object i.e. JSON.stringify(err)
+    }
+  }
+}
